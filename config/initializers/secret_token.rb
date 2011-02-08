4a76294efc6b6c1c51208998457ff9505d44ceb1
Alex Tandrau <alexandru@tandrau.com>
2011-02-08 11:47:22 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slam6DUI::Application.config.secret_token = 'ee239a0251f0e7f125abd0228e753bac62185054a2872004a98834ceafa5d5628516e80ef66096f9355194c85a6ea06629ff3da99dc12517679f82cd1164ed42'
